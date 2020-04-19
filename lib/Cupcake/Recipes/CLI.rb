115f3b3ac54b80117a89f39f04112623d5938e04
ariel murphy <arielladders@gmail.com>
2020-04-19 19:51:20 +0000

    first commit

---
+++ lib/Cupcake/Recipes/CLI.rb
@@ -0,0 +1,10 @@
+require "Cupcake/Recipes/CLI/version"
+
+module Cupcake
+  module Recipes
+    module CLI
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/Cupcake/Recipes/CLI/version.rb
@@ -0,0 +1,7 @@
+module Cupcake
+  module Recipes
+    module CLI
+      VERSION = "0.1.0"
+    end
+  end
+end
