42e09a29d99fcc8a6c49a58ce30cede5fa8f3b1b
ariel murphy <arielladders@gmail.com>
2020-04-21 19:48:21 +0000

    modified recipes

--- lib/recipes.rb
+++ lib/recipes.rb
@@ -7,10 +7,21 @@ class CupcakeRecipe
 
   attr_accessor :name, :url, :ingredients, :instructions
 
-  def initialize(name, url, category)
+  def initialize(name, url)
     @name = name
     @url = url
     @ingredients = []
     @instructions = []
     self.class.all << self
   end
+  
+  def self.all
+    @@all
+  end 
+  
+  def self.get_recipe(list)
+    self.scrape_all_recipes(list)
+    puts "Here are all of the cupcakes recipes available to you:"
+    recipe_index = Menu.display(self.type, recipes)
+  end
+    
