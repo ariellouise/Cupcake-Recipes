363ecc13525d1dda9e8945736d2d5561aae53f00
ariel murphy <arielladders@gmail.com>
2020-04-21 17:24:21 +0000

    modified scraper

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,3 +1,17 @@
+require 'open-uri'
+require 'nokogiri'
+
+
+base_url = "https://tasty.co"
+cupcakes_url = base_url + "search?q=cupcakes"
+
+
+cupcakes_page = open(cupcakes_url)
+cupcakes_html =cupcakes_page.read 
+
+parsed_cupcakes = Nokogiri::(cupcakes_html)
+
+
 class CupcakeRecipes
   
   
