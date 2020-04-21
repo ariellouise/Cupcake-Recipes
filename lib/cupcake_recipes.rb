3c89386cf2898cc8f8d26c602baf80183f86c002
ariel murphy <arielladders@gmail.com>
2020-04-21 03:54:15 +0000

    file added

---
+++ lib/cupcake_recipes.rb
@@ -0,0 +1,12 @@
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+
+module CupcakeRecipes
+  # Your code goes here...
+end
+
+require_relative "./cupcake_recipes/recipes"
+require_relative "./cupcake_recipes/scraper"
+require_relative "./cupcake_recipes/version"
+require_relative "./cupcake_recipes/cli"
