cb2cc955bb551f735f92d015fbc3e6e4df41e9f0
ariellouise <60407963+ariellouise@users.noreply.github.com>
2020-04-22 11:57:31 -0400

    Update cli.rb

--- lib/cli.rb
+++ lib/cli.rb
@@ -9,7 +9,6 @@ recipe_url = base_url + "a-big-delicious-chocolate-mess-5393121"
 categories_page = open(category_url)
 categories_html = categories_page.read
 
-
 recipe_page = open(recipe_url)
 recipe_html = recipe_page.read
 
@@ -17,16 +16,40 @@ recipe_html = recipe_page.read
 parsed_categories = Nokogiri::HTML(categories_html)
 parsed_recipe = Nokogiri::HTML(recipe_html)
 
-categories_types = parsed_categories.css("h3#a.o-Capsule__a-Headline").map(&:text)
+
 categories_list = parsed_categories.css("a").map(&:text) 
 categories_list = categories_list[70, categories_list.length - 113]
 
 ingredients_list = parsed_recipe.css("p.o-Ingredients__a-Ingredient").map(&:text)
-
 instructions_list = parsed_recipe.css("li.o-Method__m-Step").map(&:text)
 
 
 
+class RecipeComponent
 
+    @@all = [ ]
+
+    def self.
+
+    def self.
+    end
+    
+
+
+attr_reader :name, :ingredients, :instructions
+
+def initialize(name, ingredients, instructions)
+  @name, @ingredients, @instructions = name, ingredients, instructions
+  @@all << self
+end
+
+
+##all in parent class has all components mixed
+class Ingredients <RecipeComponent
+class Instructions <RecipeComponent
+
+
+#class for my recipe
+class Recipe
 
 
