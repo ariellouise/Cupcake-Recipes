af4dfecbcb4187dd396f849c5f24f8ced3776675
ariellouise <60407963+ariellouise@users.noreply.github.com>
2020-04-22 01:00:41 -0400

    Update recipes.rb

--- lib/recipes.rb
+++ lib/recipes.rb
@@ -7,11 +7,11 @@ class FN_Recipes::Recipe
 
     doc = Nokogiri::HTML(open("https://foodnetwork.com/recipes/"))
 
-     doc.css("").collect do |recipe|
-       name = recipe.css("").attribute("title").text
-       category = recipe.css("").text
-       ingredients = recipe.css("").text
-       instructions = recipe.css("").text
+     doc.css("").collect do |meal|
+       name = meal.css("").attribute("title").text
+       category = meal.css("").text
+       ingredients = meal.css("").text
+       instructions = meal.css("").text
        recipe = self.new(name, category, instructions, ingredients)
 
     end
@@ -25,56 +25,34 @@ class FN_Recipes::Recipe
     @@recipes << self
   end
 
-  def self.all
-    @@recipes.each.with_index(1) do |recipe, i|
-      puts " #{i}. #{recipe.name}"
-    end
+  def self.featured_collection
+    @@
+    puts
   end
 
-  def self.get_recipe(category)
-    self.scrape_all_recipes(category)
-    puts " You chose #{category.name} recipes:"
-    recipe_index = Menu.display(self.type, category, recipes)
-    recipe = category.recipes[recipe_index]
+  def self.just_dinner
+    @@
+    puts
   end
-    
-  def self.scrape_all_recipes(category)
-   doc = Nokogiri::HTML(open(category.url))
-   posts = doc.css("div.tve_post")
-   
-   posts.each do |post|
-    name = post.css(").text
-    url = post.css("a").attr("href").value
-    recipe = self.new(name, url, category)
-   end
+
+  def self.from_the_pantry
+    @@
+    puts
   end
   
-  def print
-   self.get_complete_recipe
-   puts "\n#{self.name} (#{self.category.name})"
-   puts "\nIngredients:"
-   self.ingredients.each do |ingredient|
-     puts (ingredient)
-   end
-   puts "\nInstructions:"
-   self.instructions.each_with_index do |instruction, index|
-     puts "#{index + 1}. #{instruction}"
-   end 
+  def self.best_recipes
+    @@
+    puts
   end
+
+  def self.trending_now
+    @@
+    puts
+  end
+
+  def self.smarter_not_harder
+    @@
+    puts
+  end
+
   
-  def get_complete_recipe
-    doc = Nokogiri::HTML(open(self.url))
-    ingredients_data = doc.css("")
-    ingredients_data.each do |ingredient|
-      amount = ingredient.css("")
-      unit = ingredient.css("")
-      name = ingredient.css("")
-      end
-      self.ingredients.push("#{amount} #{unit} #{name}")
-    end
-    
-    instructions_data = doc.css("")
-    instructions_data.each do |instructions|
-      self.instructions.push(instruction.text)
-    end
-    
