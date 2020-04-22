49b73f927bdf90e2a2181dd78a290796612a04da
ariellouise <60407963+ariellouise@users.noreply.github.com>
2020-04-22 10:22:23 -0400

    Update test.rb

--- lib/test.rb
+++ lib/test.rb
@@ -11,11 +11,8 @@ recipes_html = recipes_page.read
 parsed_recipes = Nokogiri::HTML(recipes_html)
 puts parsed_recipes.class
 #recipes_types = parsed_recipes.css("h3#a.o-Capsule__a-Headline").map(&:text)
-recipes_descriptions = parsed_recipes.css("a").map(&:text)
+recipes_descriptions = parsed_recipes.css("a").map(&:text) 
+recipes_descriptions = recipes_descriptions[70, recipes_descriptions.length - 113]
 #puts recipes_types.to_s
 #puts recipes_types.length
 puts recipes_descriptions.to_s
-
-
-
-          
