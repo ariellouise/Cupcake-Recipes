e6017fd731ad00e1a385197705795dbd95a5a3a4
ariellouise <60407963+ariellouise@users.noreply.github.com>
2020-04-21 21:46:28 -0400

    modified harvesting

--- lib/harvesting.rb
+++ lib/harvesting.rb
@@ -1,25 +1,25 @@
 require 'open-uri'
-
+require 'Nokogiri'
 
 base_url = "https://www.foodnetwork.com/"
 recipes_url = base_url + "recipes"
 
-
-page = open(recipes_url) #return instance of a tempfile class
-
-puts page.class
-puts page.content_type
-#puts page.read
-puts page.read.class
+recipes_page = open(recipes_url)
+recipes_html = recipes_page.read
 
 
+html_text = page.read #return html
 
 
-html_text = page.read #return html as a ruby string
+#filter
 
+parsed_recipes = Nokogiri::HTML(recipes_html)
 
+#puts parsed_recipes.class
 
+recipes_types = parsed_recipes.css("o-Capsule__a-HeadlineText h3").map(&:text)
+recipes_descriptions = parsed_recipes.css("m-MediaBlock__a-HeadlineText h4 ").map(&:text)
 
-
-
-
+puts recipes_types.to_s
+puts recipes_types.length
+puts recipes_descriptions.length 
