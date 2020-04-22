5e542cb996e7c2b49ffa2f359a2900946b3d1cb1
ariellouise <60407963+ariellouise@users.noreply.github.com>
2020-04-22 11:11:42 -0400

    Update select_recipes.rb

--- lib/select_recipes.rb
+++ lib/select_recipes.rb
@@ -10,9 +10,9 @@ categories_html = categories_page.read
 
 parsed_categories = Nokogiri::HTML(categories_html)
 puts parsed_categories.class
-categories_types = parsed_categories.css("h3#a.o-Capsule__a-Headline").map(&:text)
-#recipes_descriptions = parsed_categoriess.css("a").map(&:text) 
-#recipes_descriptions = recipes_descriptions[70, recipes_descriptions.length - 113]
-puts categories_types.to_s
-puts categories_types.length
-#puts recipes_descriptions.to_s
+#categories_types = parsed_categories.css("h3#a.o-Capsule__a-Headline").map(&:text)
+categories_list = parsed_categories.css("a").map(&:text) 
+categories_list = categories_list[70, categories_list.length - 113]
+#puts categories_types.to_s
+#pputs categories_types.length
+puts categories_list.to_s
