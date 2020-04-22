f2984a789a4b5ff1424de46e5dc9a7471b6ae654
ariellouise <60407963+ariellouise@users.noreply.github.com>
2020-04-21 22:55:26 -0400

    modified category.rb

--- lib/category.rb
+++ lib/category.rb
@@ -2,44 +2,48 @@
 require 'Nokogiri'
 require 'Open-uri'
 
-#base_url = "https://www.foodnetwork.com"
-#recipes_url = base_url + "recipes/"
-
 class Category
-    @@all = []
-    @@type = "category"
+  @@all = []
+  @@type = "category"
 
-    attr_accessor :name, :url, :recipes
+  attr_accessor :name, :url, :recipes
 
-    def initialize(name, url)
-        @name = name
-        @url = url
-        @recipes = []
-        self.class.all << self
+  def initialize(name, url)
+    @name = name
+    @url = url
+    @recipes = []
+    self.class.all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.type
+    @@type
+  end
+
+  def self.get_category
+    self.scrape_all_categories
+    category_index = CLI.display(self.type, self.all)
+    category = self.all[category_index]
+  end
+
+  def self.scrape_all_categories
+    main_url = "https://www.foodnetwork.com/recipes/"
+    doc = Nokogiri::HTML(open(main_url))
+    rows = doc.css("h3") 
+    rows.shift 
+
+    rows.each do |row|
+      name = row.css("h3").text
+      name.slice!("All ")
+      name.slice!(" Recipes")
+      url = row.css("a").attr("href").value
+      self.new(name, url)
     end
 
-    def self.all
-        @@all
-    end
-
-    def self.type
-        @@type
-    end
-
-    def self.get_category
-        self.scrape_all_categories
-        category_index = Menu.display(self.type, self.all)
-        category = self.all[category_index]
-    end
-
-    def self.scrape_all_categories
-        recipes_url = "https://www.foodnetwork.com/recipes/"
-        doc = Nokogiri::HTML(open(recipes_url))
-        rows = doc.css("h4").map(&:text)
-        rows.shift
-    end
-
-    def add_recipe(recipe)
-        self.recipes << recipe
-    end
+  def add_recipe(recipe)
+    self.recipes << recipe
+  end
 end
