b0a98a0e31b325cfab409ed566c8a1f7c0e8d7a2
ariellouise <60407963+ariellouise@users.noreply.github.com>
2020-04-21 20:46:18 -0400

    modified category

--- lib/category.rb
+++ lib/category.rb
@@ -1,1 +1,32 @@
-category.rb
+class Category
+    @@all = []
+    @@type = "category"
+
+    attr_accessor :name, :url, :recipes
+
+    def initialize(name, url)
+        @name = name
+        @url = url
+        @recipes = []
+        self.class.all <<self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.type
+        @@type
+    end
+
+    def self.get_category
+        self.scrape_all_categories
+        category_index = Menu.display(self.type, self.all)
+        category = self.all[category_index]
+    end
+
+    def self.scrape_all_categories
+        main_url = "https://www.foodnetwork.com/"
+        doc = Nokogiri::HTML(open(main_url))
+        
+end
