de5c49542be7a26bba56d15e48159fcee66bf181
ariellouise <60407963+ariellouise@users.noreply.github.com>
2020-04-21 22:07:48 -0400

    Update harvesting.rb

--- lib/harvesting.rb
+++ lib/harvesting.rb
@@ -2,24 +2,25 @@ require 'open-uri'
 require 'Nokogiri'
 
 base_url = "https://www.foodnetwork.com/"
-recipes_url = base_url + "recipes"
+recipes_url = base_url + "recipes/"
 
 recipes_page = open(recipes_url)
 recipes_html = recipes_page.read
 
-
-html_text = page.read #return html
 
 
 #filter
 
 parsed_recipes = Nokogiri::HTML(recipes_html)
 
-#puts parsed_recipes.class
+puts parsed_recipes.class
 
-recipes_types = parsed_recipes.css("o-Capsule__a-HeadlineText h3").map(&:text)
-recipes_descriptions = parsed_recipes.css("m-MediaBlock__a-HeadlineText h4 ").map(&:text)
+recipes_types = parsed_recipes.css("h3")
+recipes_descriptions = parsed_recipes.css("h4")
 
 puts recipes_types.to_s
 puts recipes_types.length
 puts recipes_descriptions.length 
+
+
+
