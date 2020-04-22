823fd9db7653537305d85fa19614a146299a6e9c
ariellouise <60407963+ariellouise@users.noreply.github.com>
2020-04-22 10:44:15 -0400

    modified ingredients

---
+++ lib/ingredients.rb
@@ -0,0 +1,17 @@
+require 'open-uri'
+require 'Nokogiri'
+
+
+base_url = "https://www.foodnetwork.com/recipes/"
+recipes_url = base_url + "a-big-delicious-chocolate-mess-5393121"
+
+recipes_page = open(recipes_url)
+recipes_html = recipes_page.read
+
+parsed_recipes = Nokogiri::HTML(recipes_html)
+puts parsed_recipes.class
+recipes_types = parsed_recipes.css("p.o-Ingredients__a-Ingredient").map(&:text)
+#recipes_descriptions = parsed_recipes.css("a").map(&:text) 
+#recipes_descriptions = recipes_descriptions[70, recipes_descriptions.length - 113]
+puts recipes_types.to_s
+#puts recipes_descriptions.to_s
--- lib/test.rb
+++ lib/test.rb
@@ -3,16 +3,24 @@ require 'Nokogiri'
 
 
 base_url = "https://www.foodnetwork.com/recipes/"
-recipes_url = base_url + "recipes-a-z/a"
+#recipe_url = base_url + "recipes-a-z/a"
 
-recipes_page = open(recipes_url)
-recipes_html = recipes_page.read
+#recipes_page = open(recipes_url)
+#recipes_html = recipes_page.read
 
-parsed_recipes = Nokogiri::HTML(recipes_html)
-puts parsed_recipes.class
+#parsed_recipes = Nokogiri::HTML(recipes_html)
+#puts parsed_recipes.class
 #recipes_types = parsed_recipes.css("h3#a.o-Capsule__a-Headline").map(&:text)
-recipes_descriptions = parsed_recipes.css("a").map(&:text) 
-recipes_descriptions = recipes_descriptions[70, recipes_descriptions.length - 113]
+#recipes_descriptions = parsed_recipes.css("a").map(&:text) 
+#recipes_descriptions = recipes_descriptions[70, recipes_descriptions.length - 113]
 #puts recipes_types.to_s
 #puts recipes_types.length
-puts recipes_descriptions.to_s
+#puts recipes_descriptions.to_s
+
+
+#Ingredients =      
+
+        
+recipes_url = base_url + "a-big-delicious-chocolate-mess-5393121"
+
+#recipe_ingredients = parsed_ingredients.css("o-Ingredients__a-Ingredient").map(&:text)
