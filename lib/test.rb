6f9b16f2fe2780b64325feeb0638f482df316d1e
ariellouise <60407963+ariellouise@users.noreply.github.com>
2020-04-23 02:05:37 -0400

    Update test.rb

--- lib/test.rb
+++ lib/test.rb
@@ -11,11 +11,15 @@ require 'nokogiri'
 
 #parsed_categories = Nokogiri::HTML(categories_html)
 
-#categories_list = parsed_categories.css("a:contains()~ul.m-PromoList.o-Capsule_m-PromoList li.m-PromoList_a-ListItem").map(&:text) 
+#categories_list = parsed_categories.css("a:contains()~"m-PromoList o-Capsule__m-PromoList lm-PromoList_a-ListItem").map(&:text) 
 #categories_list = categories_list[70, categories_list.length - 113]
 #puts categories_list.to_s
 
 
 #ul.m-PromoList.o-Capsule_m-PromoList
-
+#m-PromoList o-Capsule__m-PromoList
+          
+#m-PromoList__a-ListItem
 #li.m-PromoList_a-ListItem
+
+
