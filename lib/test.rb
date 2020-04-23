a52ceccbbd62c463362378f2e8c124616c39cf2a
ariellouise <60407963+ariellouise@users.noreply.github.com>
2020-04-23 01:14:27 -0400

    Nulled Test.rb

--- lib/test.rb
+++ lib/test.rb
@@ -1,15 +1,22 @@
-require 'nokogiri'
 require 'open-uri'
+require 'nokogiri'
 
 
 
-base_url = "https://www.foodnetwork.com/recipes/"
-category_url = base_url + "recipes-a-z/a"
+#base_url = "https://www.foodnetwork.com/recipes/"
+#category_url = base_url + "recipes-a-z/a"
 
-categories_page = open(category_url)
-categories_html = categories_page.read
+#categories_page = open(category_url)
+#categories_html = categories_page.read
 
-parsed_html = Nokogiri::HTML(categories_html)
-puts parsed_html.class
-categories_list = parsed_html.css("a:contains(' ')~ul.m-PromoList.o-Capsule_m-PromoList li.m-PromoList_a-ListItem")
+#parsed_categories = Nokogiri::HTML(categories_html)
+#puts parsed_categories.class
 
+#categories_list = parsed_categories.css("a:contains('a')~ul.m-PromoList.o-Capsule_m-PromoList li.m-PromoList_a-ListItem").map(&:text) 
+#categories_list = categories_list[70, categories_list.length - 113]
+#puts categories_list.to_s
+
+
+#ul.m-PromoList.o-Capsule_m-PromoList
+
+#li.m-PromoList_a-ListItem
